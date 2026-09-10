7d0cd4370cb9a4752fcc1f777afa8f00333c0b2a
zestycyzerr <zestycyzer@gmail.com>
2026-09-11 01:32:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,8 @@ const CONFIG = {
     `Okay so first of all, you absolutely ditched me for most of that Islamabad trip and I have never fully forgiven you for it, but somehow I still ended up texting you every single day since, so clearly my judgment is broken. You're annoying as fuck btw, and im sick of your age jokes. I  think about you more than any semi-sane person should think about someone they've met exactly once in real life, and then im like wow what a loser you are haha.
 
 But also like I'm kind of glad Hania decided to be a menace and make us meet, because whatever this is, brainrot and all, has turned into a very enjoyable part of my days.
-mb this is me saying it a little sideways instead of straight up, so samajh jana bas. `ap bas andaza lagao how much to me to me that if we were on a boat that was sinking, and somehow there was only one lifejacket, im obviously gonna take it; but id miss you dearly though 🥹🥹`
+mb this is me saying it a little sideways instead of straight up, so samajh jana bas.
+'ap bas andaza lagao how much to me to me that if we were on a boat that was sinking, and somehow there was only one lifejacket, im obviously gonna take it; but id miss you dearly though 🥹🥹'
 sab choro weese lmk how u liked my unique new birthday wishing tactic, THIS IS NO LESS THAN A GIFT OKAY? MERE ALL NIGHTERS LAGE HAIN U BETTER APPRECIATE THE GRINDING I DID FOR U lol.`,
     `Happy sixsevennnn birthday. I hope this year is as shit to you as were to me and
      to everyone around you (jox you werent). I'm grateful you got posted to samungli hhahaha. I hope you have a good day, and that you get to spend it with people who make you feel just as good.`
