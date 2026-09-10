890701bb0ea4b43ceb80b3750e7e63b5207c56a4
zestycyzerr <zestycyzer@gmail.com>
2026-09-11 01:57:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -46,12 +46,10 @@ sab choro weese lmk how u liked my unique new birthday wishing tactic, THIS IS N
   ],
 
   // Final screen.
-  finalHeading: "Wishing you a potentially happy and enjoyable birthday in QUETTA LOL.",
+    finalHeading: "Wishing you a potentially happy and enjoyable birthday in QUETTA LOL.",
   finalMessage:
-    "itni tareef sehat keliye achi nhi hoti hahahahh so pipe down princess" + 
-    " and have a good one ❤️ 
-     `also send me your address i may or may not ship a gift 😬. im just so kind as you ma already know HAHA`,
-
+    "itni tareef sehat keliye achi nhi hoti hahahahh so pipe down princess" +
+    " and have a good one ❤️. also send me your address base ka, i may or may not ship a gift 😬. im just so kind as you may already know HAHA",
   // Optional: set to a date string like "2022-05-14" to show a
   // "together for N days" line on the final screen. Set to null
   // to hide it entirely.
